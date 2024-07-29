b523cb69569f1a14c6663077b1ccef8ee53e6da1
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2024-07-29 19:27:32 +0300

    update terraform

--- terraform-config.tf
+++ terraform-config.tf
@@ -221,12 +221,12 @@ data "aws_eks_cluster_auth" "main" {
   name = aws_eks_cluster.main.name
 }
 
-# Kubernetes provider configuration
-provider "kubernetes" {
-  host                   = aws_eks_cluster.main.endpoint
-  token                  = data.aws_eks_cluster_auth.main.token
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-}
+# # Kubernetes provider configuration
+# provider "kubernetes" {
+#   host                   = aws_eks_cluster.main.endpoint
+#   token                  = data.aws_eks_cluster_auth.main.token
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+# }
 
 # Deploy a sample container image to EKS
 resource "kubernetes_deployment" "lbs" {
